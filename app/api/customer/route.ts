098eb6e00f2ec4f20d10929629ce765de99dc0aa
chikpan <chikpanwong@gmail.com>
2025-10-19 14:09:41 -0700

    #204

--- app/api/customer/route.ts
+++ app/api/customer/route.ts
@@ -16,6 +16,7 @@ export async function POST(req: Request) {
       );
     }
 
+    // create customer
     const newCustomer = await db.customer.create({
       data: {
         firstName: body.firstName,
