01b89014296b91aab48cebd1562bdb7937da0d1e
chikpan <chikpanwong@gmail.com>
2025-10-24 16:22:02 -0700

    # 203  add customer button

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useEffect, useState } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { CustomerGroupDropdown } from "./CustomerGroupDropdown";
+import {useEffect, useState} from "react";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {CustomerGroupDropdown} from "./CustomerGroupDropdown";
+import CreateCustomerForm from "@/components/customers/CreateCustomerForm";
 
 type CustomerGroup = "VIP" | "Repeat" | "New" | "Potential";
 
@@ -47,8 +48,8 @@ export default function CustomersPage() {
   const handleImport = async () => {
     setLoading(true);
     try {
-      await fetch("/api/customers/import", { method: "POST" });
-      await fetchCustomers(); 
+      await fetch("/api/customers/import", {method: "POST"});
+      await fetchCustomers();
     } catch (err) {
       console.error(err);
     } finally {
@@ -60,7 +61,6 @@ export default function CustomersPage() {
     fetchCustomers();
   }, []);
 
-
   return (
     <div className="p-6 space-y-6">
       {/* Import From Square Button */}
@@ -68,10 +68,10 @@ export default function CustomersPage() {
         <button
           onClick={handleImport}
           disabled={loading}
-          className="px-4 py-2 rounded border border-gray-600 text-white bg-transparent cursor-pointer"
-        >
+          className="px-4 py-2 rounded border border-gray-600 text-white bg-transparent cursor-pointer">
           {loading ? "Importing..." : "Import Customers"}
         </button>
+        <CreateCustomerForm />
       </div>
 
       {customers.length === 0 && !loading && <p>No customers found.</p>}
@@ -80,7 +80,9 @@ export default function CustomersPage() {
       {customers.map((customer) => (
         <Card key={customer.id} className="w-full p-6 shadow-md">
           <CardHeader>
-            <CardTitle>{`${customer.firstName} ${customer.lastName}`.trim()}</CardTitle>
+            <CardTitle>
+              {`${customer.firstName} ${customer.lastName}`.trim()}
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <div className="flex flex-wrap gap-6 text-gray-300 text-sm md:text-base">
@@ -96,7 +98,14 @@ export default function CustomersPage() {
                   <>
                     {customer.address
                       .map((addr) =>
-                        [addr.line1, addr.line2, addr.city, addr.state, addr.zip, addr.country]
+                        [
+                          addr.line1,
+                          addr.line2,
+                          addr.city,
+                          addr.state,
+                          addr.zip,
+                          addr.country,
+                        ]
                           .filter(Boolean)
                           .join(" ")
                       )
@@ -111,18 +120,17 @@ export default function CustomersPage() {
         </Card>
       ))}
 
-    <div className="p-6">
-      <div className="mb-6">
-        <h1 className="text-2xl font-semibold mb-4">Customers</h1>
-        <div className="flex items-center gap-4">
-          <CustomerGroupDropdown
-            selectedGroups={selectedGroups}
-            onSelectionChange={setSelectedGroups}
-          />
+      <div className="p-6">
+        <div className="mb-6">
+          <h1 className="text-2xl font-semibold mb-4">Customers</h1>
+          <div className="flex items-center gap-4">
+            <CustomerGroupDropdown
+              selectedGroups={selectedGroups}
+              onSelectionChange={setSelectedGroups}
+            />
+          </div>
         </div>
       </div>
     </div>
-    </div>
   );
-  
 }
---
+++ components/customers/CreateCustomerForm.tsx
@@ -0,0 +1,257 @@
+"use client";
+
+import {useState} from "react";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {Textarea} from "@/components/ui/textarea";
+import {toast} from "sonner";
+import {Plus} from "lucide-react";
+
+export interface AddressProps {
+  line1: string;
+  line2?: string;
+  city: string;
+  state: string;
+  zip: string;
+  country: string;
+}
+
+interface FormCustomerProps {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber?: string;
+  additionalNote?: string;
+  squareId?: string;
+  address: AddressProps;
+}
+
+export default function CreateCustomerForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState<FormCustomerProps>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    additionalNote: "",
+    address: {
+      line1: "",
+      line2: "",
+      city: "",
+      state: "",
+      zip: "",
+      country: "",
+    },
+  });
+
+  // https://www.meje.dev/blog/handle-change-in-ts
+  // (handleChange in TypeScript)
+  // handle change for normal inputs
+  function handleInputChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const {name, value} = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  function handleAddressChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const {name, value} = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      address: {
+        // keep other address fields
+        ...prev.address,
+        // update the specific field
+        [name]: value,
+      },
+    }));
+  }
+
+  // handle reset form
+  function handleResetForm() {
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: "",
+      additionalNote: "",
+      address: {
+        line1: "",
+        line2: "",
+        city: "",
+        state: "",
+        zip: "",
+        country: "",
+      },
+    });
+  }
+
+  // handle submit
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("/api/customer", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          // Prisma expects an array
+          address: [formData.address],
+        }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        toast.error(data.message || "Something went wrong");
+        return;
+      }
+
+      if (response.ok) {
+        toast.success(data.message);
+        handleResetForm();
+      } else {
+        toast.error("Failed to create customer");
+      }
+    } catch (error) {
+      console.error("Error creating customer:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus /> Add Customer
+        </Button>
+      </DialogTrigger>
+
+      <DialogPortal>
+        <DialogOverlay />
+
+        <DialogContent className="top-[6%] left-[33%] overflow-y-auto">
+          <DialogHeader>
+            <DialogTitle>Add New Customer</DialogTitle>
+            <DialogDescription>
+              Fill in the customer details below.
+            </DialogDescription>
+          </DialogHeader>
+
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <Input
+              name="firstName"
+              placeholder="First Name"
+              value={formData.firstName}
+              onChange={handleInputChange}
+              required
+            />
+            <Input
+              name="lastName"
+              placeholder="Last Name"
+              value={formData.lastName}
+              onChange={handleInputChange}
+              required
+            />
+            <Input
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+            <Input
+              name="phoneNumber"
+              placeholder="Phone Number"
+              value={formData.phoneNumber}
+              onChange={handleInputChange}
+              required
+            />
+            <Textarea
+              name="additionalNote"
+              placeholder="Additional Note"
+              value={formData.additionalNote}
+              onChange={handleInputChange}
+            />
+
+            <Label>Address</Label>
+            <Input
+              name="line1"
+              placeholder="Line 1"
+              value={formData.address.line1}
+              onChange={handleAddressChange}
+              required
+            />
+            <Input
+              name="line2"
+              placeholder="Line 2"
+              value={formData.address.line2}
+              onChange={handleAddressChange}
+            />
+            <Input
+              name="city"
+              placeholder="City"
+              value={formData.address.city}
+              onChange={handleAddressChange}
+              required
+            />
+            <Input
+              name="state"
+              placeholder="State"
+              value={formData.address.state}
+              onChange={handleAddressChange}
+              required
+            />
+            <Input
+              name="zip"
+              placeholder="ZIP"
+              value={formData.address.zip}
+              onChange={handleAddressChange}
+              required
+            />
+            <Input
+              name="country"
+              placeholder="Country"
+              value={formData.address.country}
+              onChange={handleAddressChange}
+              required
+            />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="destructive" onClick={handleResetForm}>
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button variant="default" type="submit" disabled={isLoading}>
+                {isLoading ? "Saving..." : "Create Customer"}
+              </Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </DialogPortal>
+    </Dialog>
+  );
+}
